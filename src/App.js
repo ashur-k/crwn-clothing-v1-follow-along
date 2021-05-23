301018aeb5181351e994bb8bd2811d2d7bba3055
ashur-k <ashurkanwal@yahoo.com>
2021-05-23 22:14:24 +0100

    created new components  / + form components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component.jsx'
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact  path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     
        
