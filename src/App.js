7fb2668b750b4cd1f2401b6f7b00a2f2b511c9a0
ashur-k <ashurkanwal@yahoo.com>
2021-05-23 20:12:33 +0100

    product added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
-const HatsPage = (props) => { 
+import ShopPage from './pages/shop/shop.component.jsx'
 
-  return(
-  
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
-  }
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact  path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     
        
