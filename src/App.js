1abb58c3a2ddebaec47e20708420301f52059c24
ashur-k <ashurkanwal@yahoo.com>
2021-05-23 16:45:37 +0100

    added routing structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
+const HatsPage = (props) => { 
+
+  return(
+  
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+  }
+
 function App() {
   return (
     <div>
-        <HomePage />
+      <Switch>
+        <Route exact  path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
+    
+       
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
  
     <div
         className={`${size} menu-item`}
+        onClick ={ () => history.push(`${match.url}${linkUrl}`) } 
     >
         <div className='background-image' style={{
             backgroundImage: `url(${imageUrl})`
@@ -19,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   
   );
 
-  export default MenuItem;
+  export default withRouter(MenuItem);
