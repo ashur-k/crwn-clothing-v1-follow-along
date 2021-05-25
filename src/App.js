013abd80bad4e3e52bba3b029b5a1153e00ca563
ashur-k <ashurkanwal@yahoo.com>
2021-05-25 01:04:42 +0100

    Firebase google user added to firebase data collections

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component.jsx'
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -22,13 +22,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       
-      this.setState({ currentUser: user });
-      console.log(user);
+     if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+            this.setState({
+                currentUser: {
+                  id: snapShot.id,
+                  ...snapShot.data()
+                }
+            });
+        });
+       
+     }    
+     this.setState({ currentUser: userAuth });
 
     })
-  }
+  };
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
