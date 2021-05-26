be10d864553c48093ef4f910117b79f86231de6e
ashur-k <ashurkanwal@yahoo.com>
2021-05-26 20:24:21 +0100

    implmented cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,11 +4,12 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import './header.style.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -32,11 +33,16 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
+        {
+            hidden ? null :
+            <CartDropdown />
+        }
     </div>  
   );
 
-  const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+  const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
   })
 
 
