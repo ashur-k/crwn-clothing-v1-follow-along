05c9f866c774b6892b815d4cda14afe74891e103
ashur-k <ashurkanwal@yahoo.com>
2021-05-29 13:48:44 +0100

    updated cartitem page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect'
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
-import './checkout.styles.scss'
+import { createStructuredSelector } from 'reselect';
+    
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import './checkout.styles.scss';
 
 const CheckoutPage = ({cartItems, total}) => (
     
@@ -26,9 +29,8 @@ const CheckoutPage = ({cartItems, total}) => (
         </div>
         {
             cartItems.map(cartItem => 
-                cartItem.name
-                )
-        }
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+                )}
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
