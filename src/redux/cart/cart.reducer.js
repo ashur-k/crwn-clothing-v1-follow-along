43cca0356ef15348d333547d080d13210f701063
ashur-k <ashurkanwal@yahoo.com>
2021-05-29 14:43:45 +0100

    git add / remove increase / decrease bag quantity functionality is working

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionsTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -12,12 +12,26 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return{
                 ...state,
                 hidden: !state.hidden
-            }
+            };
         case CartActionsTypes.ADD_ITEM:
             return{
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
             };
+        case CartActionsTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id 
+                )
+            };
+        
+        case CartActionsTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+
         default:
             return state;
     }
