8e6c46c50122966a0e14c1d4b307571d536f9eb0
ashur-k <ashurkanwal@yahoo.com>
2021-06-06 11:44:03 +0100

    updated

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,7 +5,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
-    const publishableKey = process.env.publishableKey
+    const publishableKey = 'pk_test_51HNEO8K0aexytnqPxW65qgoIwUMKU5hZQ45M3LPdVTQMrzep3Mgtnr2cbZ5w4A3JIUOCXxaR40kzwCaqgUw89WcQ00OKaCOAkz'
     
     const onToken = token => {
         console.log(token);
