77df51875908cfb884849e9f42821b9267745678
ashur-k <ashurkanwal@yahoo.com>
2021-06-30 23:51:47 +0100

    installed redux-saga, and created shop.sage file, implemented saga middleware in store.js learnt about function* (star functions) and how yeild keyword get used and we write controled funciton which we can run aysnc

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,31 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from './shop.actions';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+  yield console.log('I am firebase');
+
+  try{
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap))
+  }catch (error){
+    yield put(fetchCollectionsFailure(error.message))
+  }
+
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+   )
+}
