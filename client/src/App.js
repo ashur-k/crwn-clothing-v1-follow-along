60894642cc05c2b57763b2acff1eb028d4442b1e
ashur-k <ashurkanwal@yahoo.com>
2021-07-20 20:17:20 +0100

    added css

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import { connect } from 'react-redux';
 
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component.jsx';
+
+import { GlobalStyle } from './global.styles';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions'
@@ -24,6 +25,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
     return (
       <div>
+        <GlobalStyle />
         <Header />
         <Switch>
           <Route exact  path='/' component={HomePage} />
