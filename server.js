e0d3849de018221107b1ab4023dadd9015a39f47
ashur-k <ashurkanwal@yahoo.com>
2021-07-21 17:19:51 +0100

    adding compression library

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const compression = require('compression')
+
 if (process.env.NODE_ENV != 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
@@ -10,6 +12,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
