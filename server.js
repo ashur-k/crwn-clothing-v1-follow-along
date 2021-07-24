b73843385abee1991b891b76d42780127a4208c5
ashur-k <ashurkanwal@yahoo.com>
2021-07-24 10:39:30 +0100

    updated server.js

--- server.js
+++ server.js
@@ -13,16 +13,19 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression);
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-if(process.env.NODE_ENV === 'production') {  
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+
+if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, 'client/build', index.html))
   });
 }
 
